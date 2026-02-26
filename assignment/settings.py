8ca2f1ccf77522c6ce370392028c8379b9a421fc
Koushalichavva <24b01a1218@svecw.edu.in>
2026-02-26 21:50:05 +0530

    modified settings

--- assignment/settings.py
+++ assignment/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1i32m@bq05v*n$-nc2rl%!x5(#!a&1!i-thc(=^tz)l%ck@h71
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['koushali.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
